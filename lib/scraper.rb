6967b6a3600156b7b7c2dc23b6be5262a6227338
jstone18 <jason.stone0808@gmail.com>
2019-01-15 23:55:31 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,29 @@
 require 'nokogiri'
 require 'open-uri'
 
+
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +33,13 @@ class Scraper
       end
     end
   end
-  
+
 end
 
+# Scraper.new.print_courses
+# Scraper.new.get_page
 
-
+# doc.css("post")
+# doc.css(".post").first.css("h2").text #=> "Web Development Immersive"
+# doc.css(".post").first.css(".date").text #=> "Full-Time"
+# doc.css(".post").first.css("p").text #=> "An intensive, Ruby and Javascript course that teaches the skills necessary to start a career as a full-stack software developer."
